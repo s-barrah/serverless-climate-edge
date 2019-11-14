376bd2c3aaf48355dc655da283b82f1eff7b5b0d
Sidney Barrah <neothedeveloper231@gmail.com>
2019-11-14 22:02:00 +0000

    Cleaned up file upload action

--- src/Action/FileUpload/FileUpload.action.js
+++ src/Action/FileUpload/FileUpload.action.js
@@ -14,7 +14,6 @@ import ChildResolver from '../../Resolver/Child.resolver';
 import UserResolver from '../../Resolver/User.resolver';
 import PlotResolver from '../../Resolver/Plot.resolver';
 
-import stationMock from '../../../tests/mocks/form/station/valid.mock.json';
 
 /***
  * Upload files
@@ -28,8 +27,7 @@ import stationMock from '../../../tests/mocks/form/station/valid.mock.json';
  * @apiSuccess {string} message The overall system health
  *
  */
-import CONFIGURATION, { DEFINITIONS, TABLES, TABLE_DEFINITIONS } from '../../Config/Configuration';
-import StationModel from "../../Model/Station.model";
+import CONFIGURATION, { DEFINITIONS, TABLES } from '../../Config/Configuration';
 
 export default LambdaWrapper(CONFIGURATION, async (di: DependencyInjection, request: RequestService, done) => {
   let response = {};
@@ -41,52 +39,45 @@ export default LambdaWrapper(CONFIGURATION, async (di: DependencyInjection, requ
 
   if (formData) {
 
-    const filesCount = Object.values(formData).length;
+    const data = new FileParserService().getFileContent(formData); // get data from files
 
-    console.log('filesCount: ', filesCount);
-
-    const data = new FileParserService().getFileContent(formData);
-
+    // PROCESS AGE DATA
     const ageResolver = new AgeResolver(di);
-    const varietyResolver = new VarietyResolver(di);
-    const plotResolver = new PlotResolver(di);
-    const childResolver = new ChildResolver(di);
-    const userResolver = new UserResolver(di);
-
     ageResolver.importFromFile(data); // get import data
-
     const ages = ageResolver.getRawData(); // hydrate and return entities
-
-    // PROCESS AGES
     await di.get(DEFINITIONS.DATABASE).processBatches(ages, TABLES.AGE_TABLE); // store to db
 
-    // PROCESS VARIETIES
+    // PROCESS VARIETIES DATA
+    const varietyResolver = new VarietyResolver(di);
     varietyResolver.importFromFile(data); // get import data
     const varieties = await varietyResolver.getRawData(); // hydrate and return entities
     await di.get(DEFINITIONS.DATABASE).processBatches(varieties, TABLES.VARIETY_TABLE); // store to db
 
 
-    // PROCESS PLOTS
+    // PROCESS PLOTS DATA
+    const plotResolver = new PlotResolver(di);
     plotResolver.importFromFile(data); // get import data
     const plots = await plotResolver.getRawData(); // hydrate and return entities
     await di.get(DEFINITIONS.DATABASE).processBatches(plots, TABLES.PLOT_TABLE); // store to db
 
-    // PROCESS CHILDREN
+    // PROCESS CHILDREN DATA
+    const childResolver = new ChildResolver(di);
     childResolver.importFromFile(data); // get import data
     const children = await childResolver.getChildren(); // hydrate and return entities
     await di.get(DEFINITIONS.DATABASE).processBatches(children, TABLES.CHILD_TABLE); // store to db
 
-    const childrenResults = await childResolver.getAll();
 
-    // PROCESS USERS
+    // PROCESS USERS DATA
+    const userResolver = new UserResolver(di);
     userResolver.importFromFile(data); // get import data
     const users = await userResolver.getRawData(); // hydrate and return entities
     await di.get(DEFINITIONS.DATABASE).processBatches(users, TABLES.USER_TABLE); // store to db
 
-    const userResults = await userResolver.getAll();
-
+    const filesCount = Object.values(formData).length;
     const message = filesCount > 1 ? `${filesCount} files` : `1 file`;
+
     response = new ResponseModel({}, 200, `Successfully processed ${message}`);
+
   } else {
     response = new ResponseModel({}, 500, 'No files found!');
   }
